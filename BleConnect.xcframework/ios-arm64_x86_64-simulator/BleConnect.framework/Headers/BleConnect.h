f68e58a3bd8f587e50bf3deeb6a062ad550e05b1
Albin CR <albinzr@gmail.com>
2021-09-27 14:03:08 +0530

    first commit

---
+++ BleConnect.xcframework/ios-arm64_x86_64-simulator/BleConnect.framework/Headers/BleConnect.h
@@ -0,0 +1,19 @@
+//
+//  BleConnect.h
+//  BleConnect
+//
+//  Created by albin on 21/09/21.
+//
+
+#import <Foundation/Foundation.h>
+
+
+//! Project version number for BleConnect.
+FOUNDATION_EXPORT double BleConnectVersionNumber;
+
+//! Project version string for BleConnect.
+FOUNDATION_EXPORT const unsigned char BleConnectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BleConnect/PublicHeader.h>
+
+

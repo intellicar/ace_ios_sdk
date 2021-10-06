3a17d440a9e384d99870d0236beff31c2b153301
runner <runner@Mac-1633509240919.local>
2021-10-06 08:36:00 +0000

    Updating SDK 0.0.13 to 0.0.14

--- BleConnect.xcframework/ios-arm64/BleConnect.framework/Headers/BleConfig.swift
+++ BleConnect.xcframework/ios-arm64/BleConnect.framework/Headers/BleConfig.swift
@@ -9,18 +9,21 @@ import Foundation
 import CoreBluetooth
 
 public struct BleConfig{
-   
-   let deviceUuids:[CBUUID] = [CBUUID(string: "60910001-eef7-4b20-a0be-cf7a163415fc")]
-   let read:CBUUID = CBUUID(string: "60910003-EEF7-4B20-A0BE-CF7A163415FC")
-   let write:CBUUID = CBUUID(string:"60910002-EEF7-4B20-A0BE-CF7A163415FC")
-   public var scanTimeout:TimeInterval = 30
-   public var connectionTimeout:TimeInterval = 30
-   public var isDebugEnabled = false
     
-    public init(isDebugEnabled:Bool=false,scanTimeout:TimeInterval = 30,connectionTimeout:TimeInterval = 30) {
+    let deviceUuids:[CBUUID] = [CBUUID(string: "60910001-eef7-4b20-a0be-cf7a163415fc")]
+    let read:CBUUID = CBUUID(string: "60910003-EEF7-4B20-A0BE-CF7A163415FC")
+    let write:CBUUID = CBUUID(string:"60910002-EEF7-4B20-A0BE-CF7A163415FC")
+    
+    public let name:String
+    public var scanTimeout:TimeInterval = 30
+    public var connectionTimeout:TimeInterval = 30
+    public var isDebugEnabled = false
+    
+    public init(isDebugEnabled:Bool=false,scanTimeout:TimeInterval = 30,connectionTimeout:TimeInterval = 30,name:String) {
         self.isDebugEnabled = isDebugEnabled
         self.scanTimeout = scanTimeout
         self.connectionTimeout = connectionTimeout
+        self.name = name
         
     }
 }

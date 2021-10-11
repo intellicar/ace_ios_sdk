f1b648c87537423b2304e1aa02b3c65f5aa2c6ab
runner <runner@Mac-1633954978752.local>
2021-10-11 12:30:23 +0000

    Updating SDK 0.0.27 to 0.0.28

--- BleConnect.xcframework/ios-arm64/BleConnect.framework/Headers/BleConfig.swift
+++ BleConnect.xcframework/ios-arm64/BleConnect.framework/Headers/BleConfig.swift
@@ -13,11 +13,16 @@ public struct BleConfig{
     let deviceUuids:[CBUUID] = [CBUUID(string: "60910001-eef7-4b20-a0be-cf7a163415fc")]
     let read:CBUUID = CBUUID(string: "60910003-EEF7-4B20-A0BE-CF7A163415FC")
     let write:CBUUID = CBUUID(string:"60910002-EEF7-4B20-A0BE-CF7A163415FC")
-    
+    //
+    public var isDebugEnabled = false
+    public var rawBeaconDebug = false
+    public var LA5BeaconDebug = false
+    //
     public let name:String
     public var scanTimeout:TimeInterval = 30
     public var connectionTimeout:TimeInterval = 30
-    public var isDebugEnabled = false
+    
+    
     
     public init(isDebugEnabled:Bool=false,scanTimeout:TimeInterval = 30,connectionTimeout:TimeInterval = 30,name:String) {
         self.isDebugEnabled = isDebugEnabled
